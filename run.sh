b8d4586535a57144b0c4fd1851b0a8522a70db78
O-SeHwan <tpghks1106@naver.com>
2025-02-13 17:01:45 +0900

    modify

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
-python3 ./angrpt.py -d ~/drivers/exovol.sys --user-static 0x11368 
+#!/bin/bash
+
+driver=$1
+address=$2
+dirpath="result/$(basename $driver .sys)"
+logpath="result/$(basename $driver .sys)/angrPT.log"
+
+echo "Driver Name: $driver"
+echo "User Static Address: $address"
+echo "Log file path: $logpath"
+mkdir -p "$dirpath"
+
+python3 ./angrpt.py -d $driver --user-static $address | tee $logpath
